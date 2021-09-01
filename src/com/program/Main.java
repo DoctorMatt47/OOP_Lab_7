5ad6d5913cd10a6b14781f12bf7afbf91b23207f
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-09-01 14:47:05 +0300

    Project - Added main

---
+++ src/com/program/Main.java
@@ -0,0 +1,12 @@
+package com.program;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
