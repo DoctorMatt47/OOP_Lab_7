d832799036c176accd8601d75c6e3c2447ba25c2
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-10-08 19:29:36 +0300

    domain.core - Changed apartment builder

--- src/OOP_Lab_7/program/core/builder/ApartmentBuilder.java
+++ src/OOP_Lab_7/program/core/builder/ApartmentBuilder.java
@@ -16,13 +16,20 @@ public class ApartmentBuilder implements IApartmentBuilder {
 
     private void buildAppliances(Apartment apartment) {
         var appliances = apartment.getAppliances();
-        appliances.add(new Fridge(11, new Dimension(80, 100, 193.5), 310, 360));
-        appliances.add(new Fridge(12, new Dimension(80, 100, 193.5), 310, 360));
-        appliances.add(new AirConditioner(21, new Dimension(63.5, 45.8, 24), 233, 730));
-        appliances.add(new AirConditioner(22, new Dimension(72, 49.5, 27), 164, 680));
-        appliances.add(new Microwave(31, new Dimension(37.4, 48.9, 48.9), 1150, 800));
-        appliances.add(new Oven(41, new Dimension(56.6, 59.5, 45.5), 1200, 1000));
-        appliances.add(new Vacuum(51, new Dimension(35, 35, 81.5), 40, 1));
+        appliances.add(new Fridge(11, new Dimension(80, 100, 193.5),
+                1411, 360));
+        appliances.add(new Fridge(12, new Dimension(80, 100, 193.5),
+                1200 , 360));
+        appliances.add(new AirConditioner(21, new Dimension(63.5, 45.8, 24),
+                24000, 730));
+        appliances.add(new AirConditioner(22, new Dimension(72, 49.5, 27),
+                10000, 680));
+        appliances.add(new Microwave(31, new Dimension(37.4, 48.9, 48.9),
+                1000, 800));
+        appliances.add(new Oven(41, new Dimension(56.6, 59.5, 45.5),
+                1200, 1000));
+        appliances.add(new Vacuum(51, new Dimension(35, 35, 81.5),
+                1000, 1));
     }
 
     private void buildOutlets(Apartment apartment) {
