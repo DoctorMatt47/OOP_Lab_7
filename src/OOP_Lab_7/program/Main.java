14ff88c8ad8f88cd65c70fd0a3146aa43debbe51
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-10-08 19:32:13 +0300

    program - Changed main method

--- src/OOP_Lab_7/program/Main.java
+++ src/OOP_Lab_7/program/Main.java
@@ -1,8 +1,13 @@
 package OOP_Lab_7.program;
 
+import OOP_Lab_7.program.core.builder.ConsoleApplicationBuilder;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        new ConsoleApplicationBuilder().build().show();
     }
 }
