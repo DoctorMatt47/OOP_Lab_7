6e0a7ecd90b03834c7f47410e00eb61e08c591b6
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-10-08 18:04:35 +0300

    domain.core - Added apartment builder

---
+++ src/OOP_Lab_7/program/core/builder/ApartmentBuilder.java
@@ -0,0 +1,35 @@
+package OOP_Lab_7.program.core.builder;
+
+import OOP_Lab_7.program.domain.entity.apartment.Apartment;
+import OOP_Lab_7.program.domain.entity.appliance.*;
+import OOP_Lab_7.program.domain.entity.socket.ElectricOutlet;
+import OOP_Lab_7.program.domain.valueobject.Dimension;
+
+public class ApartmentBuilder implements IApartmentBuilder {
+
+    public Apartment build() {
+        var apartment = new Apartment();
+        buildAppliances(apartment);
+        buildOutlets(apartment);
+        return apartment;
+    }
+
+    private void buildAppliances(Apartment apartment) {
+        var appliances = apartment.getAppliances();
+        appliances.add(new Fridge(11, new Dimension(80, 100, 193.5), 310, 360));
+        appliances.add(new Fridge(12, new Dimension(80, 100, 193.5), 310, 360));
+        appliances.add(new AirConditioner(21, new Dimension(63.5, 45.8, 24), 233, 730));
+        appliances.add(new AirConditioner(22, new Dimension(72, 49.5, 27), 164, 680));
+        appliances.add(new Microwave(31, new Dimension(37.4, 48.9, 48.9), 1150, 800));
+        appliances.add(new Oven(41, new Dimension(56.6, 59.5, 45.5), 1200, 1000));
+        appliances.add(new Vacuum(51, new Dimension(35, 35, 81.5), 40, 1));
+    }
+
+    private void buildOutlets(Apartment apartment) {
+        var outlets = apartment.getOutlets();
+        int COUNT_OF_OUTLET = 3;
+        for (int i = 0; i < COUNT_OF_OUTLET; i++) {
+            outlets.add(new ElectricOutlet());
+        }
+    }
+}
---
+++ src/OOP_Lab_7/program/core/builder/IApartmentBuilder.java
@@ -0,0 +1,7 @@
+package OOP_Lab_7.program.core.builder;
+
+import OOP_Lab_7.program.domain.entity.apartment.Apartment;
+
+public interface IApartmentBuilder {
+    Apartment build();
+}
